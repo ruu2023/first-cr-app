e176d79327efb6389c43ac1373ef2c1ce28a4dbe
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-07-28 18:59:42 +0900

    temp

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test3', function () {
+Route::get('/test', function () {
     return 'test';
 });
